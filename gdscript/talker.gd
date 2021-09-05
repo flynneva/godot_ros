e434541ab2bd6a08950a1dadcb4dfec6f9dfa40f
Evan Flynn <evanflynn.msu@gmail.com>
2021-09-05 11:34:45 -0700

    update comments

--- gdscript/talker.gd
+++ gdscript/talker.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+# initialize timeout signal
 signal timeout
 
 # calls rclcpp::init(0, nullptr) to make sure ROS is started up
@@ -11,21 +12,21 @@ var count = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# spin nodes that were added to executors
-	# not really need for talker demo but will be needed later for subscribing
-	# to ROS2 topics
+	# initialize ros node
 	ros_talker.spin_some()
 	
 	g_timer = Timer.new()
 	add_child(g_timer)
-	# connect the talk signal with the talk callback
+	# connect the talk signal with the timeout callback
 	g_timer.connect("timeout", self, "_on_Timer_timeout")
 	g_timer.set_wait_time(1.0)
 	g_timer.set_one_shot(false)
 	g_timer.start()
-	pass # Replace with function body.
+	pass
 
+# called on timeout signal
 func _on_Timer_timeout():
   print("godot is talking: " + String(count))
+  # talk callback publishes to ROS
   ros_talker.talk(count)
   count += 1
--- gdscript/viewport_publisher.gd
+++ gdscript/viewport_publisher.gd
@@ -1,28 +1,31 @@
 extends Spatial
 
+# initialize timeout signal
 signal timeout
 var g_timer = null
 var count = 1
 
+# initialize ROS viewport node
 var ros_viewport = ViewPort.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
-	# spin node
+	# start up ROS node
 	ros_viewport.spin_some()
 	
 	g_timer = Timer.new()
 	add_child(g_timer)
-	# connect the talk signal with the talk callback
+	# connect the talk signal with the timeout callback
 	g_timer.connect("timeout", self, "_on_Timer_timeout")
 	g_timer.set_wait_time(1.0)
 	g_timer.set_one_shot(false)
 	g_timer.start()
 	pass # Replace with function body.
 
+# called on timeout signal
 func _on_Timer_timeout():
 	print("godot viewport publishing" + String(count))
+
 	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
 	# Wait until the frame has finished before getting the texture.
 	yield(VisualServer, "frame_post_draw")
@@ -30,5 +33,6 @@ func _on_Timer_timeout():
 	var img = get_viewport().get_texture().get_data()
 	img.flip_y()
 	img.convert(Image.FORMAT_RGB8)
+	# publish image to ROS
 	ros_viewport.pubImage(img)
 	count += 1
