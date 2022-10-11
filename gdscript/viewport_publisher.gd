1266a5f4d77690ebcd6b1c2e155a2ecc6c8cf9ae
RDaneelOlivaw <duckfrost@gmail.com>
2022-10-11 16:51:14 +0200

    Added example for Godot4 new structure viewport

--- gdscript/viewport_publisher.gd
+++ gdscript/viewport_publisher.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends Node3D
 
 # initialize timeout signal
 signal timeout
@@ -13,25 +13,14 @@ func _ready():
 	# start up ROS node
 	ros_viewport.spin_some()
 	
-	g_timer = Timer.new()
-	add_child(g_timer)
-	# connect the talk signal with the timeout callback
-	g_timer.connect("timeout", self, "_on_Timer_timeout")
-	g_timer.set_wait_time(1.0)
-	g_timer.set_one_shot(false)
-	g_timer.start()
-	pass # Replace with function body.
 
-# called on timeout signal
-func _on_Timer_timeout():
-	print("godot viewport publishing" + String(count))
+func _on_timer_timeout():
+	# print("godot viewport publishing" + String.chr(count))
 
-	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
 	# Wait until the frame has finished before getting the texture.
-	yield(VisualServer, "frame_post_draw")
+	await RenderingServer.frame_post_draw
 	# Retrieve the captured image.
-	var img = get_viewport().get_texture().get_data()
-	img.flip_y()
+	var img = get_viewport().get_texture().get_image()
 	img.convert(Image.FORMAT_RGB8)
 	# publish image to ROS
 	ros_viewport.pubImage(img)
